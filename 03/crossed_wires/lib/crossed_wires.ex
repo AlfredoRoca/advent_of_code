c0cb724c6db9cbcdb176a2bb11252a1b8ff4fcfe
Alfredo <alfredo.roca.mas@gmail.com>
2020-04-09 21:11:45 +0200

    day 03 part 2 - wrong answer

--- 03/crossed_wires/lib/crossed_wires.ex
+++ 03/crossed_wires/lib/crossed_wires.ex
@@ -8,13 +8,14 @@ defmodule CrossedWires do
   end
 
   def minimum_distance_of_crossing_points(route1, route2) do
-    all_points_without_starting_point =
-      (Enum.uniq(route1) ++ Enum.uniq(route2))
-      |> Enum.reject(fn point -> point == {0, 0} end)
-
-    crossing_points_in(all_points_without_starting_point)
+    crossing_points_in(all_points_without_starting_point(route1, route2))
     |> Enum.map(fn {x, y} -> abs(x) + abs(y) end)
     |> Enum.min()
+  end
+
+  def all_points_without_starting_point(route1, route2) do
+    (Enum.uniq(route1) ++ Enum.uniq(route2))
+    |> Enum.reject(fn point -> point == {0, 0} end)
   end
 
   def crossing_points_in(all_points) do
@@ -32,6 +33,8 @@ defmodule CrossedWires do
       end)
 
     List.flatten(route)
+    |> Enum.uniq()
+    |> Enum.reject(fn point -> point == {x0, y0} end)
   end
 
   def get_coordinates(direction, length, x0, y0) when direction == "R" do
@@ -52,5 +55,20 @@ defmodule CrossedWires do
   def get_coordinates(direction, length, x0, y0) when direction == "D" do
     {points, new_y0} = Enum.map_reduce(0..length, y0, fn _y, y0 -> {{x0, y0}, y0 - 1} end)
     {points, {0, new_y0 - y0 + 1}}
+  end
+
+  def min_combined_steps([path1, path2]) do
+    route1 = get_points(path1, {0, 0})
+    route2 = get_points(path2, {0, 0})
+
+    all_points_without_starting_point(route1, route2)
+    |> crossing_points_in()
+    |> Enum.map(fn point -> combined_steps_to_intersection(point, route1, route2) end)
+    |> Enum.min()
+  end
+
+  def combined_steps_to_intersection(intersection, route1, route2) do
+    Enum.find_index(route1, fn point -> point == intersection end) + 1 +
+      Enum.find_index(route2, fn point -> point == intersection end) + 1
   end
 end
--- 03/crossed_wires/test/crossed_wires_test.exs
+++ 03/crossed_wires/test/crossed_wires_test.exs
@@ -8,11 +8,32 @@ defmodule CrossedWiresTest do
     assert CrossedWires.nearest_cross(paths) == distance
   end
 
+  test "get the points of paths 1" do
+    path = "R8,U5,L5,D3"
+    quantity = 21
+
+    assert CrossedWires.get_points(path, {0, 0}) |> Enum.count() == quantity
+  end
+
+  test "the shortest path for paths 1" do
+    paths = ["R8,U5,L5,D3", "U7,R6,D4,L4"]
+    min_combined_steps = 30
+
+    assert CrossedWires.min_combined_steps(paths) == min_combined_steps
+  end
+
   test "the nearest for paths 2" do
     distance = 159
     paths = ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]
 
     assert CrossedWires.nearest_cross(paths) == distance
+  end
+
+  test "the shortest path for paths 2" do
+    paths = ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]
+    min_combined_steps = 610
+
+    assert CrossedWires.min_combined_steps(paths) == min_combined_steps
   end
 
   test "the nearest for paths 3" do
@@ -26,6 +47,17 @@ defmodule CrossedWiresTest do
     assert CrossedWires.nearest_cross(paths) == distance
   end
 
+  test "the shortest path for paths 3" do
+    paths = [
+      "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+      "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+    ]
+
+    min_combined_steps = 410
+
+    assert CrossedWires.min_combined_steps(paths) == min_combined_steps
+  end
+
   test "the nearest for paths in the file" do
     distance = 1626
 
@@ -34,5 +66,16 @@ defmodule CrossedWiresTest do
       |> String.split("\n", trim: true)
 
     assert CrossedWires.nearest_cross(paths) == distance
+  end
+
+  test "the shortest path for paths in the file" do
+    paths =
+      File.read!("paths.txt")
+      |> String.split("\n", trim: true)
+
+    # 1st attempt not correct -> 27326
+    min_combined_steps = 0
+
+    assert CrossedWires.min_combined_steps(paths) == min_combined_steps
   end
 end
