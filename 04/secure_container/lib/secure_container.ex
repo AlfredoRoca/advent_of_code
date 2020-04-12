ca6a78b59a00ecbbeae282bfe878dfe092bc4e46
Alfredo <alfredo.roca.mas@gmail.com>
2020-04-12 10:22:42 +0200

    day 04 part 2

--- 04/secure_container/lib/secure_container.ex
+++ 04/secure_container/lib/secure_container.ex
@@ -1,4 +1,6 @@
 defmodule SecureContainer do
+  import Integer
+
   def how_many_pw(range) do
     range
     |> get_pws()
@@ -13,7 +15,8 @@ defmodule SecureContainer do
   end
 
   def meets_criteria?(x) do
-    its_a_6_digit_number?(x) && digits_never_decrease?(x) && contains_adjacent_doubles(x)
+    its_a_6_digit_number?(x) && digits_never_decrease?(x) && contains_adjacent_doubles(x) &&
+      at_least_one_adjacent_pair(x)
   end
 
   def its_a_6_digit_number?(x) do
@@ -48,5 +51,29 @@ defmodule SecureContainer do
     |> Enum.reduce_while("", fn new, last ->
       if new == last, do: {:halt, new}, else: {:cont, new}
     end) != "a"
+  end
+
+  def at_least_one_adjacent_pair(x) do
+    digits =
+      x
+      |> Integer.to_string()
+      |> String.split("", trim: true)
+
+    {_, _, meets} =
+      [digits | ["a"]]
+      |> List.flatten()
+      |> Enum.reduce_while({"", 0, false}, fn new, {last, counter, meets} ->
+        if new == last do
+          {:cont, {new, counter + 1, false}}
+        else
+          if counter == 1 do
+            {:halt, {new, 0, true}}
+          else
+            {:cont, {new, 0, false}}
+          end
+        end
+      end)
+
+    meets
   end
 end
--- 04/secure_container/test/secure_container_test.exs
+++ 04/secure_container/test/secure_container_test.exs
@@ -1,24 +1,65 @@
 defmodule SecureContainerTest do
   use ExUnit.Case
 
-  test "sample meets criteria" do
-    pw = 111_111
+  test "sample 1 meets criteria" do
+    pw = 112_233
     assert SecureContainer.meets_criteria?(pw) == true
   end
 
-  test "smaple does not meet criteria due decreasing digits" do
+  test "sample 2 meets criteria" do
+    pw = 111_122
+    assert SecureContainer.meets_criteria?(pw) == true
+  end
+
+  @tag t: true
+  test "sample 3 meets criteria" do
+    pw = 112_222
+    assert SecureContainer.meets_criteria?(pw) == true
+  end
+
+  test "sample 4 meets criteria" do
+    pw = 111_223
+    assert SecureContainer.meets_criteria?(pw) == true
+  end
+
+  test "sample does not meet criteria due decreasing digits" do
     pw = 223_450
     assert SecureContainer.meets_criteria?(pw) == false
   end
 
-  test "smaple does not meet criteria due no double digits" do
+  test "sample does not meet criteria due no double digits" do
     pw = 123_789
+    assert SecureContainer.meets_criteria?(pw) == false
+  end
+
+  test "sample 1 does not meet criteria due double digits not even number" do
+    pw = 111_234
+    assert SecureContainer.meets_criteria?(pw) == false
+  end
+
+  test "sample 2 does not meet criteria due double digits not even number" do
+    pw = 123_444
+    assert SecureContainer.meets_criteria?(pw) == false
+  end
+
+  test "sample 3 does not meet criteria due double digits not even number" do
+    pw = 144_444
+    assert SecureContainer.meets_criteria?(pw) == false
+  end
+
+  test "sample 4 does not meet criteria due double digits not even number" do
+    pw = 333_444
+    assert SecureContainer.meets_criteria?(pw) == false
+  end
+
+  test "sample 5 does not meet criteria due no double" do
+    pw = 111_111
     assert SecureContainer.meets_criteria?(pw) == false
   end
 
   test "greets the world" do
     input_range = 147_981..691_423
-    quantity = 1790
+    quantity = 1206
     assert SecureContainer.how_many_pw(input_range) == quantity
   end
 end
