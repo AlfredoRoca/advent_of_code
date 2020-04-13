c36d04a3ee8ac0932d64dbd744a985053fef37c7
Alfredo <alfredo.roca.mas@gmail.com>
2020-04-13 17:27:04 +0200

    Day 05 part 1

--- 05/intcode_computer/lib/intcode_computer.ex
+++ 05/intcode_computer/lib/intcode_computer.ex
@@ -1,4 +1,13 @@
 defmodule IntcodeComputer do
+  require Logger
+
+  @position_mode "0"
+  @immediate_mode "1"
+
+  def run_diagnostic_program(program) do
+    run(program)
+  end
+
   def solution_to_program_1202 do
     find_solution_for_program(12, 2)
   end
@@ -42,16 +51,22 @@ defmodule IntcodeComputer do
       |> Enum.map(&String.to_integer/1)
       |> Enum.slice(3..-1)
 
-    ([1, noun, verb] ++ instructions)
-    |> run()
+    {_result, program, _exit_code} =
+      ([1, noun, verb] ++ instructions)
+      |> run()
+
+    program
     |> String.split(",")
     |> List.first()
     |> String.to_integer()
   end
 
   def run(program) when is_list(program) do
-    run_step(program, 0)
-    |> Enum.join(",")
+    {result, data, exit_code} = run_step(program, 0)
+
+    case result do
+      :halt -> {:halt, data |> Enum.join(","), exit_code}
+    end
   end
 
   def run(program) when is_binary(program) do
@@ -61,23 +76,64 @@ defmodule IntcodeComputer do
     |> run()
   end
 
-  def run_step(prog, index) do
-    opcode = Enum.fetch!(prog, index)
+  def run_step(prog, index, exit_code \\ 0) do
+    instruction = Enum.fetch!(prog, index)
+    opcode = get_opcode_from_instruction(instruction)
 
     case opcode do
       n when n in [1, 2] ->
-        new_prog = execute_opcode(opcode, prog, index)
-        run_step(new_prog, index + 4)
+        {prog, index} = execute_opcode_for_aritmethics(instruction, prog, index)
+        run_step(prog, index)
+
+      3 ->
+        {prog, index} = execute_opcode_for_external_input(instruction, prog, index)
+        run_step(prog, index)
+
+      4 ->
+        {prog, index, exit_code} = execute_opcode_for_external_output(instruction, prog, index)
+        run_step(prog, index, exit_code)
 
       99 ->
-        prog
+        {:halt, prog, exit_code}
+
+      _ ->
+        run_step(prog, index + 1)
     end
   end
 
-  def execute_opcode(opcode, prog, index) do
+  def execute_opcode_for_external_input(_instruction, prog, index) do
+    # returns the new prog and the next position in the prog
+    input =
+      IO.gets("Introduce integer: ")
+      |> String.trim()
+      |> String.to_integer()
+
+    target = Enum.fetch!(prog, index + 1)
+    {insert_value_into_prog_at_position(prog, target, input), index + 2}
+  end
+
+  def execute_opcode_for_external_output(_instruction, prog, index) do
+    # returns the prog, the next position in the prog and the value output
+    target = Enum.fetch!(prog, index + 1)
+    exit_code = Enum.fetch!(prog, target)
+    Logger.info("Output exit_code: #{exit_code}")
+    {prog, index + 2, exit_code}
+  end
+
+  def execute_opcode_for_aritmethics(instruction, prog, index) do
+    # returns the new prog and the next position in the prog
+    opcode =
+      instruction
+      |> get_opcode_from_instruction()
+
+    {mode_param1, mode_param2} =
+      instruction
+      |> get_modes_from_instruction()
+
     [_pos0, pos1, pos2, pos3] = Enum.slice(prog, index..(index + 3))
-    value1 = Enum.fetch!(prog, pos1)
-    value2 = Enum.fetch!(prog, pos2)
+
+    value1 = fetch_value(prog, pos1, mode_param1)
+    value2 = fetch_value(prog, pos2, mode_param2)
 
     operation =
       case opcode do
@@ -85,10 +141,37 @@ defmodule IntcodeComputer do
         2 -> value1 * value2
       end
 
-    if pos3 == 0 do
-      [operation] ++ Enum.slice(prog, 1..-1)
-    else
-      Enum.slice(prog, 0..(pos3 - 1)) ++ [operation] ++ Enum.slice(prog, (pos3 + 1)..-1)
-    end
+    {insert_value_into_prog_at_position(prog, pos3, operation), index + 4}
+  end
+
+  defp get_opcode_from_instruction(instruction) do
+    rem(instruction, 100)
+  end
+
+  defp get_modes_from_instruction(instruction) do
+    <<mode_param2::binary-size(1), mode_param1::binary-size(1)>> =
+      trunc(instruction / 100)
+      |> Integer.to_string()
+      |> String.pad_leading(2, "0")
+
+    {mode_param1, mode_param2}
+  end
+
+  defp fetch_value(prog, pos, mode) when mode == @position_mode do
+    Enum.fetch!(prog, pos)
+  end
+
+  defp fetch_value(_prog, pos, mode) when mode == @immediate_mode do
+    pos
+  end
+
+  def insert_value_into_prog_at_position(prog, pos, value) when pos == 0 do
+    # insert at the begining
+    [value] ++ Enum.slice(prog, 1..-1)
+  end
+
+  def insert_value_into_prog_at_position(prog, pos, value) when pos != 0 do
+    # insert in between
+    Enum.slice(prog, 0..(pos - 1)) ++ [value] ++ Enum.slice(prog, (pos + 1)..-1)
   end
 end
--- 05/intcode_computer/test/intcode_computer_test.exs
+++ 05/intcode_computer/test/intcode_computer_test.exs
@@ -1,46 +1,131 @@
 defmodule IntcodeComputerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+  import Mock
+  require Logger
 
+  @tag t: true
+  test "program auto halting" do
+    program = "1002,4,3,4,33"
+    output = "1002,4,3,4,99"
+    {_result, program, _exit_code} = IntcodeComputer.run(program)
+    assert program == output
+  end
+
+  @tag t: true
+  test "program with console input" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "50\n"
+        end
+      end
+    ) do
+      program = "3,2,0,99"
+      output = "3,2,50,99"
+      {_result, program, _exit_code} = IntcodeComputer.run(program)
+      assert program == output
+    end
+  end
+
+  @tag t: true
+  test "program with console output" do
+    Logger.info("Running program with output...")
+
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "50\n"
+        end
+      end
+    ) do
+      program = "3,0,4,0,99"
+      output = "50,0,4,0,99"
+
+      {_result, program, _exit_code} = IntcodeComputer.run(program)
+
+      assert program == output
+    end
+  end
+
+  @tag t: true
+  test "sample program with negative integers" do
+    program = "1101,100,-1,4,0"
+    output = "1101,100,-1,4,99"
+
+    {_result, program, _exit_code} = IntcodeComputer.run(program)
+    assert program == output
+  end
+
+  @tag t: true
+  @tag t1: true
+  test "diagnostic program" do
+    Logger.info("Running diagnostic program...")
+    program = File.read!("TEST_diagnostic_program.txt")
+    output = 9_219_874
+
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "1\n"
+        end
+      end
+    ) do
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == output
+    end
+  end
+
+  #
+  #
+  # tests for computer day 02
+  #
+  #
   test "runs program 1" do
     program = "1,0,0,0,99"
     output = "2,0,0,0,99"
 
-    assert IntcodeComputer.run(program) == output
+    {_result, program, _exit_code} = IntcodeComputer.run(program)
+    assert program == output
   end
 
   test "runs program 2" do
     program = "2,3,0,3,99"
     output = "2,3,0,6,99"
 
-    assert IntcodeComputer.run(program) == output
+    {_result, program, _exit_code} = IntcodeComputer.run(program)
+    assert program == output
   end
 
   test "runs program 3" do
     program = "2,4,4,5,99,0"
     output = "2,4,4,5,99,9801"
 
-    assert IntcodeComputer.run(program) == output
+    {_result, program, _exit_code} = IntcodeComputer.run(program)
+    assert program == output
   end
 
   test "runs program 4" do
     program = "1,1,1,4,99,5,6,0,99"
     output = "30,1,1,4,2,5,6,0,99"
 
-    assert IntcodeComputer.run(program) == output
+    {_result, program, _exit_code} = IntcodeComputer.run(program)
+    assert program == output
   end
 
   test "runs program 5" do
     program = "1,9,10,3,2,3,11,0,99,30,40,50"
     output = "3500,9,10,70,2,3,11,0,99,30,40,50"
 
-    assert IntcodeComputer.run(program) == output
+    {_result, program, _exit_code} = IntcodeComputer.run(program)
+    assert program == output
   end
 
   test "runs program 6" do
     program = File.read!("program.txt")
     output = File.read!("solution_to_program_file.txt")
 
-    assert IntcodeComputer.run(program) == output
+    {_result, program, _exit_code} = IntcodeComputer.run(program)
+    assert program == output
   end
 
   test "runs program 1202" do
