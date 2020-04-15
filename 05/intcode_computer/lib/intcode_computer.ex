915138555eebfe43404b7be4950da75e6fb77211
Alfredo <alfredo.roca.mas@gmail.com>
2020-04-16 00:13:11 +0200

    day 05 part 2

--- 05/intcode_computer/lib/intcode_computer.ex
+++ 05/intcode_computer/lib/intcode_computer.ex
@@ -71,6 +71,9 @@ defmodule IntcodeComputer do
 
   def run(program) when is_binary(program) do
     program
+    |> String.split("\n", trim: true)
+    |> Enum.join(",")
+    |> String.replace(",,", ",")
     |> String.split(",", trim: true)
     |> Enum.map(&String.to_integer/1)
     |> run()
@@ -81,16 +84,20 @@ defmodule IntcodeComputer do
     opcode = get_opcode_from_instruction(instruction)
 
     case opcode do
-      n when n in [1, 2] ->
-        {prog, index} = execute_opcode_for_aritmethics(instruction, prog, index)
+      n when n in [1, 2, 7, 8] ->
+        {prog, index} = execute_opcode_for_operations(prog, index)
         run_step(prog, index)
 
       3 ->
-        {prog, index} = execute_opcode_for_external_input(instruction, prog, index)
+        {prog, index} = execute_opcode_for_external_input(prog, index)
         run_step(prog, index)
 
       4 ->
         {prog, index, exit_code} = execute_opcode_for_external_output(instruction, prog, index)
+        run_step(prog, index, exit_code)
+
+      n when n in [5, 6] ->
+        {prog, index} = execute_opcode_for_jump(prog, index)
         run_step(prog, index, exit_code)
 
       99 ->
@@ -101,7 +108,28 @@ defmodule IntcodeComputer do
     end
   end
 
-  def execute_opcode_for_external_input(_instruction, prog, index) do
+  def execute_opcode_for_operations(prog, index) do
+    {opcode, value1, value2, param3} = get_instruction_data(prog, index)
+
+    operation =
+      case opcode do
+        1 ->
+          value1 + value2
+
+        2 ->
+          value1 * value2
+
+        7 ->
+          if value1 < value2, do: 1, else: 0
+
+        8 ->
+          if value1 == value2, do: 1, else: 0
+      end
+
+    {insert_value_into_prog_at_position(prog, param3, operation), index + 4}
+  end
+
+  def execute_opcode_for_external_input(prog, index) do
     # returns the new prog and the next position in the prog
     input =
       IO.gets("Introduce integer: ")
@@ -109,19 +137,38 @@ defmodule IntcodeComputer do
       |> String.to_integer()
 
     target = Enum.fetch!(prog, index + 1)
+
     {insert_value_into_prog_at_position(prog, target, input), index + 2}
   end
 
-  def execute_opcode_for_external_output(_instruction, prog, index) do
-    # returns the prog, the next position in the prog and the value output
-    target = Enum.fetch!(prog, index + 1)
-    exit_code = Enum.fetch!(prog, target)
-    Logger.info("Output exit_code: #{exit_code}")
+  def execute_opcode_for_external_output(instruction, prog, index) do
+    {mode_param1, _mode_param2} =
+      instruction
+      |> get_modes_from_instruction()
+
+    source = Enum.fetch!(prog, index + 1)
+    exit_code = fetch_value(prog, source, mode_param1)
+
     {prog, index + 2, exit_code}
   end
 
-  def execute_opcode_for_aritmethics(instruction, prog, index) do
-    # returns the new prog and the next position in the prog
+  def execute_opcode_for_jump(prog, index) do
+    {opcode, value1, value2, _param3} = get_instruction_data(prog, index)
+
+    new_index =
+      case opcode do
+        # jump-if-true
+        5 -> if value1 != 0, do: value2, else: index + 3
+        # jump-if-false
+        6 -> if value1 == 0, do: value2, else: index + 3
+      end
+
+    {prog, new_index}
+  end
+
+  defp get_instruction_data(prog, index) do
+    [instruction, param1, param2, param3] = Enum.slice(prog, index..(index + 3))
+
     opcode =
       instruction
       |> get_opcode_from_instruction()
@@ -130,18 +177,10 @@ defmodule IntcodeComputer do
       instruction
       |> get_modes_from_instruction()
 
-    [_pos0, pos1, pos2, pos3] = Enum.slice(prog, index..(index + 3))
+    value1 = fetch_value(prog, param1, mode_param1)
+    value2 = fetch_value(prog, param2, mode_param2)
 
-    value1 = fetch_value(prog, pos1, mode_param1)
-    value2 = fetch_value(prog, pos2, mode_param2)
-
-    operation =
-      case opcode do
-        1 -> value1 + value2
-        2 -> value1 * value2
-      end
-
-    {insert_value_into_prog_at_position(prog, pos3, operation), index + 4}
+    {opcode, value1, value2, param3}
   end
 
   defp get_opcode_from_instruction(instruction) do
--- 05/intcode_computer/test/intcode_computer_test.exs
+++ 05/intcode_computer/test/intcode_computer_test.exs
@@ -3,34 +3,278 @@ defmodule IntcodeComputerTest do
   import Mock
   require Logger
 
-  @tag t: true
-  test "program auto halting" do
-    program = "1002,4,3,4,33"
-    output = "1002,4,3,4,99"
-    {_result, program, _exit_code} = IntcodeComputer.run(program)
-    assert program == output
-  end
+  #
+  #
+  # tests part 2
+  #
+  #
 
-  @tag t: true
-  test "program with console input" do
+  test "sample program 1 (position mode) - equal -> true" do
     with_mock(IO,
       gets: fn prompt ->
         case prompt do
-          "Introduce integer: " -> "50\n"
+          "Introduce integer: " -> "8\n"
         end
       end
     ) do
-      program = "3,2,0,99"
-      output = "3,2,50,99"
-      {_result, program, _exit_code} = IntcodeComputer.run(program)
-      assert program == output
+      program = "3,9,8,9,10,9,4,9,99,-1,8"
+      expected_code = 1
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
     end
   end
 
-  @tag t: true
-  test "program with console output" do
-    Logger.info("Running program with output...")
+  test "sample program 1 (immediate mode) - equal -> true" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "8\n"
+        end
+      end
+    ) do
+      program = "3,3,1108,-1,8,3,4,3,99"
+      expected_code = 1
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
 
+  test "sample program 1 (position mode) - equal -> false" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "3\n"
+        end
+      end
+    ) do
+      program = "3,9,8,9,10,9,4,9,99,-1,8"
+      expected_code = 0
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 1 (immediate mode) - equal -> false" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "3\n"
+        end
+      end
+    ) do
+      program = "3,3,1108,-1,8,3,4,3,99"
+      expected_code = 0
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 2 (position mode) - less than -> true" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "3\n"
+        end
+      end
+    ) do
+      program = "3,9,7,9,10,9,4,9,99,-1,8"
+      expected_code = 1
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 2 (position mode) - less than -> false" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "42\n"
+        end
+      end
+    ) do
+      program = "3,9,7,9,10,9,4,9,99,-1,8"
+      expected_code = 0
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 2 (immediate mode) - less than -> true" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "3\n"
+        end
+      end
+    ) do
+      program = "3,3,1107,-1,8,3,4,3,99"
+      expected_code = 1
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "variant program 2 (immediate mode) - less than -> true" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "3\n"
+        end
+      end
+    ) do
+      program = "3,3,1107,-1,8,3,4,3,99"
+      new_prog = "3,3,1107,1,8,3,4,3,99"
+      {_result, program, exit_code} = IntcodeComputer.run(program)
+      assert program == new_prog
+      assert exit_code == 1
+    end
+  end
+
+  test "sample program 2 (immediate mode) - less than -> false" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "42\n"
+        end
+      end
+    ) do
+      program = "3,3,1107,-1,8,3,4,3,99"
+      expected_code = 0
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 3 (position mode) - jump input 0 " do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "0\n"
+        end
+      end
+    ) do
+      program = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9"
+      expected_code = 0
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 3 (position mode) - jump input non 0" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "42\n"
+        end
+      end
+    ) do
+      program = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9"
+      expected_code = 1
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 3 (immediate mode) - jump input 0 " do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "0\n"
+        end
+      end
+    ) do
+      program = "3,3,1105,-1,9,1101,0,0,12,4,12,99,1"
+      expected_code = 0
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 3 (immediate mode) - jump input non 0" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "42\n"
+        end
+      end
+    ) do
+      program = "3,3,1105,-1,9,1101,0,0,12,4,12,99,1"
+      expected_code = 1
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 4 - combined below 8" do
+    # The above example program uses an input instruction to ask for a single number.
+    # The program will then output 999 if the input value is below 8,
+    # output 1000 if the input value is equal to 8,
+    # or output 1001 if the input value is greater than 8.
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "2\n"
+        end
+      end
+    ) do
+      program = """
+      3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,
+      21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+      """
+
+      expected_code = 999
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 4 - combined equals 8" do
+    # The above example program uses an input instruction to ask for a single number.
+    # The program will then output 999 if the input value is below 8,
+    # output 1000 if the input value is equal to 8,
+    # or output 1001 if the input value is greater than 8.
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "8\n"
+        end
+      end
+    ) do
+      program = """
+      3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,
+      21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+      """
+
+      expected_code = 1000
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "sample program 4 - combined above 8" do
+    # The above example program uses an input instruction to ask for a single number.
+    # The program will then output 999 if the input value is below 8,
+    # output 1000 if the input value is equal to 8,
+    # or output 1001 if the input value is greater than 8.
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "42\n"
+        end
+      end
+    ) do
+      program = """
+      3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,
+      21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+      """
+
+      expected_code = 1001
+      {_result, _program, exit_code} = IntcodeComputer.run(program)
+      assert exit_code == expected_code
+    end
+  end
+
+  test "program with console output (position mode)" do
     with_mock(IO,
       gets: fn prompt ->
         case prompt do
@@ -47,7 +291,67 @@ defmodule IntcodeComputerTest do
     end
   end
 
-  @tag t: true
+  test "program with console output (inmediate mode)" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "50\n"
+        end
+      end
+    ) do
+      program = "3,0,104,42,99"
+      output = "50,0,104,42,99"
+      expected_code = 42
+
+      {_result, program, exit_code} = IntcodeComputer.run(program)
+
+      assert exit_code == expected_code
+      assert program == output
+    end
+  end
+
+  @tag final: true
+  test "diagnostic program for the ship's thermal radiator controller - System ID 5" do
+    Logger.info(
+      "Running TEST diagnostic program for the ship's thermal radiator controller - System ID 5\n Introduce integer 5..."
+    )
+
+    program = File.read!("TEST_diagnostic_program.txt")
+
+    expected_code = 5_893_654
+
+    {_result, _program, exit_code} = IntcodeComputer.run(program)
+    assert exit_code == expected_code
+    Logger.info("Exit code ship's thermal radiator controller diagnostic program = #{exit_code}")
+  end
+
+  #
+  #
+  # tests part 1
+  #
+  #
+  test "program auto halting" do
+    program = "1002,4,3,4,33"
+    output = "1002,4,3,4,99"
+    {_result, program, _exit_code} = IntcodeComputer.run(program)
+    assert program == output
+  end
+
+  test "program with console input" do
+    with_mock(IO,
+      gets: fn prompt ->
+        case prompt do
+          "Introduce integer: " -> "50\n"
+        end
+      end
+    ) do
+      program = "3,2,0,99"
+      output = "3,2,50,99"
+      {_result, program, _exit_code} = IntcodeComputer.run(program)
+      assert program == output
+    end
+  end
+
   test "sample program with negative integers" do
     program = "1101,100,-1,4,0"
     output = "1101,100,-1,4,99"
@@ -56,10 +360,8 @@ defmodule IntcodeComputerTest do
     assert program == output
   end
 
-  @tag t: true
-  @tag t1: true
   test "diagnostic program" do
-    Logger.info("Running diagnostic program...")
+    Logger.info("Running TEST diagnostic program for system ID 1...")
     program = File.read!("TEST_diagnostic_program.txt")
     output = 9_219_874
 
@@ -72,6 +374,7 @@ defmodule IntcodeComputerTest do
     ) do
       {_result, _program, exit_code} = IntcodeComputer.run(program)
       assert exit_code == output
+      Logger.info("Exit code TEST diagnostic program for system ID 1 = #{exit_code}")
     end
   end
 
