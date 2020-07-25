c4288225af3a4077471f78dbffdacb790573d852
Alfredo <alfredo.roca.mas@gmail.com>
2020-07-25 12:49:04 +0200

    test additional computer functionalities

--- 09/sensor_boost/test/intcode_computer_test.exs
+++ 09/sensor_boost/test/intcode_computer_test.exs
@@ -4,6 +4,30 @@ defmodule IntcodeComputerTest do
   import Mock
 
   @tag f: true
+  test "can handle large numbers - 3" do
+    program = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+    expected_result = 1_125_899_906_842_624
+
+    {_result, _program, exit_code, _relative_base} = IntcodeComputer.run(program)
+    assert exit_code == expected_result
+  end
+
+  test "can handle large numbers - 1" do
+    program = "104,1125899906842624,99"
+    expected_result = 1_125_899_906_842_624
+
+    {_result, _program, exit_code, _relative_base} = IntcodeComputer.run(program)
+    assert exit_code == expected_result
+  end
+
+  test "can handle large numbers - 2" do
+    program = "1102,34915192,34915192,7,4,7,99,0"
+    expected_result = 1_219_070_632_396_864
+
+    {_result, _program, exit_code, _relative_base} = IntcodeComputer.run(program)
+    assert exit_code == expected_result
+  end
+
   test "relative base offset - 1" do
     program = "109,19,99"
     relative_base = 2000
